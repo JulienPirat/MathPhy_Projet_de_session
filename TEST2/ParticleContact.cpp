e68084d7805d3318b3b09ce9b7c744ab86d82fa6
TinyTheRealCoder <benj.trebor@gmail.com>
2023-10-10 11:48:23 -0400

    Add Particle Contact h

---
+++ TEST2/ParticleContact.cpp
@@ -0,0 +1,18 @@
+#include "ParticleContact.h"
+
+void ParticleContact::resolve(float duration)
+{
+}
+
+float ParticleContact::calculateSeparatingVelocity()
+{
+	return 0.0f;
+}
+
+void ParticleContact::resolveVelocity()
+{
+}
+
+void ParticleContact::resolveInterpenetration()
+{
+}

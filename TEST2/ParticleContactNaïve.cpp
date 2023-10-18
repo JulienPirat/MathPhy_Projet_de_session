027ed60317a8627ddf148c76c7cdc91531805622
TinyTheRealCoder <benj.trebor@gmail.com>
2023-10-18 18:02:27 -0400

    Naive

---
+++ TEST2/ParticleContactNaïve.cpp
@@ -0,0 +1,6 @@
+#include "ParticleContactNaïve.h"
+
+unsigned int ParticleContactNaïve::addContact(ParticleContact* contact, unsigned int limit) const
+{
+    return 0;
+}
--- TEST2/ParticleContactNaïve.h
+++ TEST2/ParticleContactNaïve.h
@@ -1,6 +1,17 @@
 #pragma once
-class ParticleContactNaïve
-{
+#include <vector>
+#include <Particle.h>
+#include "ParticleContactGenerator.h"
 
+class ParticleContactNaïve : public ParticleContactGenerator
+{
+	//Radius de la particule
+	float radius;
+	
+	//Particles qui pourraient collisioner ensemble
+	std::vector<Particle*> particle;
+
+	//take a pointer to an array of contacts and the number of contact left to be generated
+	virtual unsigned int addContact(ParticleContact* contact, unsigned int limit) const;
 };
 

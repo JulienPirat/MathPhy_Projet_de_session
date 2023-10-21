b06259346cb10fbcfcae5fd246f09d744be53a78
BaKo <b.kotte@laposte.net>
2023-10-21 18:43:15 -0400

    Feat : Added the definition for the method ResolveContacts

--- TEST2/ParticleContactResolver.cpp
+++ TEST2/ParticleContactResolver.cpp
@@ -2,5 +2,25 @@
 
 void ParticleContactResolver::resolveContacts(ParticleContact* contactArray, unsigned int numContact, float duration)
 {
+	iterationsUsed = 0;
+	while (iterationsUsed < iteration)
+	{
+		//Find the contact with the largest closing velocity
+		float max = 0;
+		unsigned int maxIndex = numContact;
+		for (unsigned int i = 0; i < numContact; i++)
+		{
+			float sepVel = contactArray[i].calculateSeparatingVelocity();
+			if (sepVel < max && (sepVel < 0 || contactArray[i].penetration > 0))
+			{
+				max = sepVel;
+				maxIndex = i;
+			}
+		}
 
+		//Resolve this contact
+		contactArray[maxIndex].resolve(duration);
+
+		iterationsUsed++;
+	}
 }
--- TEST2/ParticleContactResolver.h
+++ TEST2/ParticleContactResolver.h
@@ -7,7 +7,12 @@ protected:
 	//number of allowed iterations
 	unsigned int iteration;
 
+	//Track the number of iterations used
+	unsigned iterationsUsed;
+
 public: 
+	//Creates a new contact resolver with the number of max iterations
+	ParticleContactResolver(unsigned iterations) : iteration(iterations) {};
 
 	//take an array of contact
 	//iterate on each contact according to their separatingVelocity

db9c5f5eb84125806aff98feff5223210d507d9a
JulienPirat <72934794+JulienPirat@users.noreply.github.com>
2023-10-23 16:01:58 -0400

    [ADD] - Update AnchoredSpring avec Loi de Hooke

--- TEST2/ParticleAnchoredSpring.cpp
+++ TEST2/ParticleAnchoredSpring.cpp
@@ -1,1 +1,20 @@
 #include "ParticleAnchoredSpring.h"
+
+void ParticleAnchoredSpring::UpdateForce(Particle* particle, float duration)
+{
+	// Calculer le vecteur de la particule à l'ancrage
+	Vector3D anchorToParticle = particle->getPosition() - m_anchor;
+
+	// Calculer la longueur actuelle du ressort
+	float longueurActuelle = anchorToParticle.magnitude();
+
+	// Calculer la magnitude de la force en utilisant la loi de Hooke
+	double magnitudeForce = -m_k * (longueurActuelle - m_restLength);
+
+	// Calculer le vecteur de force
+	anchorToParticle.operator*=(magnitudeForce);
+
+	Vector3D ForceRessorLoiHooke = anchorToParticle;
+
+	particle->addForce(ForceRessorLoiHooke);
+}

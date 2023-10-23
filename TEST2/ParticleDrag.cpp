d0cc6e78ba69cb6e6d910972a52268ebf86471db
BaKo <b.kotte@laposte.net>
2023-10-23 19:35:37 -0400

    Fix : Spring was a duplicate of drag

--- TEST2/ParticleDrag.cpp
+++ TEST2/ParticleDrag.cpp
@@ -1,1 +1,14 @@
 #include "ParticleDrag.h"
+
+void ParticleDrag::UpdateForce(Particle* particle, float duration)
+{
+	//Calculate the total drag coefficient
+	float dragCoeff = particle->getVelocity().magnitude();
+	dragCoeff = m_k1 * dragCoeff + m_k2 * dragCoeff * dragCoeff;
+
+	//Calculate the final force and apply it
+	Vector3D force = particle->getVelocity();
+	force.normalize();
+	force *= -dragCoeff;
+	particle->addForce(force);
+}

c30e88983652e5c43c7f19e37b720bd11bcd7548
JulienPirat <72934794+JulienPirat@users.noreply.github.com>
2023-10-23 20:15:01 -0400

    suppr commentaire

--- TEST2/ParticleGravity.cpp
+++ TEST2/ParticleGravity.cpp
@@ -2,8 +2,6 @@
 
 void ParticleGravity::UpdateForce(Particle* particle, float duration)
 {
-	// POURQUOI Pourquoi on a une duration ?
-
 	Vector3D newGravityForce = Vector3D(m_gravity.x * particle->getInverseMass(), m_gravity.y * particle->getInverseMass(), m_gravity.z * particle->getInverseMass());
 	particle->addForce(newGravityForce);
 }

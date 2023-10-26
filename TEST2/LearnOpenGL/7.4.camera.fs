bb3f210b6643bd1ddcfca15931df7d0af923aa1a
BaKo <b.kotte@laposte.net>
2023-10-25 23:04:45 -0400

    Feat : The world is full of life and color
    (except for Julien)

--- TEST2/LearnOpenGL/7.4.camera.fs
+++ TEST2/LearnOpenGL/7.4.camera.fs
@@ -1,14 +1,10 @@
 #version 330 core
-out vec4 FragColor;
 
-in vec2 TexCoord;
+uniform vec3 objectColor;
 
-// texture samplers
-uniform sampler2D texture1;
-uniform sampler2D texture2;
+out vec3     FragColor;
 
 void main()
 {
-	// linearly interpolate between both textures (80% container, 20% awesomeface)
-	FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
+    FragColor = objectColor;
 }

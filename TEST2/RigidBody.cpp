93eaec1f8db991a299e01d97e9f88812446dc96a
TinyTheRealCoder <benj.trebor@gmail.com>
2023-10-30 09:16:12 -0400

    Update RB

--- TEST2/RigidBody.cpp
+++ TEST2/RigidBody.cpp
@@ -5,6 +5,26 @@ void RigidBody::Integrate(float duration)
 	//TODO
 }
 
+void RigidBody::AddForce(const Vector3D& force)
+{
+	//TODO
+}
+
+void RigidBody::AddForceAtPoint(const Vector3D& force, const Vector3D& worldPoint)
+{
+	//TODO
+}
+
+void RigidBody::AddForceAtBodyPoint(const Vector3D& force, const Vector3D& localPoint)
+{
+	//TODO
+}
+
+void RigidBody::ClearAccumulator()
+{
+	//TODO
+}
+
 void RigidBody::CalculateDerivedData()
 {
 	//TODO
--- TEST2/RigidBody.h
+++ TEST2/RigidBody.h
@@ -23,12 +23,33 @@ private :
 	//Calculates transform matrix from orientation and rotation
 	Matrix4 transformMatrix;
 
+	//Same as linear damping but for rotation
+	float m_angularDamping;
+
+	//Accumulate force added by Force Generator
+	Vector3D m_forceAccum;
+
+	//Accumulated torque added by Force Generator
+	Vector3D m_torqueAccum;
+
 public:
 
 	///Methodes
 	
 	//Integrate the rigidbody by modifying position, orientation and velocity;
 	void Integrate(float duration);
+
+	//Add Force on the center of mass (no torque generated)
+	void AddForce(const Vector3D& force);
+
+	//Add force at a point in world coordinate. Generated force and torque
+	void AddForceAtPoint(const Vector3D& force, const Vector3D& worldPoint);
+
+	//Add force at a point in local coordinate. The point is converted in world coordinated by using the transform matrix. Generate force and torque.
+	void AddForceAtBodyPoint(const Vector3D& force, const Vector3D& localPoint);
+
+	//Called each frame to reset 
+	void ClearAccumulator();
 
 private:
 

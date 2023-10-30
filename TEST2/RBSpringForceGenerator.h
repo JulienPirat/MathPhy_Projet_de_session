2cb25abe568c71f02a26621ffd83afad06ac1432
JulienPirat <72934794+JulienPirat@users.noreply.github.com>
2023-10-30 11:05:35 -0400

    Changement des commentaires

--- TEST2/RBSpringForceGenerator.h
+++ TEST2/RBSpringForceGenerator.h
@@ -3,19 +3,21 @@
 class RBSpringForceGenerator : public RBForceGenerator {
 
 private:
+
 	// Anchor point in Local coordinate
 	Vector3D m_bodyAnchor;
 
-	RigidBody* m_otherRigidBody = nullptr;
 	// Other anchor point in other's Local coordinate
 	Vector3D m_otherBodyAnchor;
 
+	RigidBody* m_otherRigidBody = nullptr;
+
 	// spring parameters
 	float m_k;
-	float m_restiLenght;
+	float m_restLenght;
 
 public:
 	// Transform each anchor point in world coordinate,
-	// calculate the spring froce and apply it at anchor point.
+	// calculate the spring force and apply it at anchor point.
 	void UpdateForce(RigidBody* rigidBody);
 };

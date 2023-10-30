224cf14ddeceb6d522a2dc7930e870c3c35f9b58
JulienPirat <72934794+JulienPirat@users.noreply.github.com>
2023-10-30 09:46:51 -0400

    Add RBGravity update and put public RB attributes

--- TEST2/RBGravityForceGenerator.cpp
+++ TEST2/RBGravityForceGenerator.cpp
@@ -2,5 +2,9 @@
 
 void RBGravityForceGenerator::UpdateForce(RigidBody* rigidBody)
 {
-	// TODO
+	//TODO si rigidBody masse Infini return;
+
+	m_gravity.operator*=(rigidBody->inverseMasse);
+
+	rigidBody->AddForce(m_gravity);
 }

e26ac9e59b72cca2a17f7ee478fa6745b8082157
JulienPirat <72934794+JulienPirat@users.noreply.github.com>
2023-12-16 18:25:39 -0500

    Update RBContactResolver.cpp

--- TEST2/RBContactResolver.cpp
+++ TEST2/RBContactResolver.cpp
@@ -18,7 +18,6 @@ void RBContactResolver::resolveContacts(RBContactRegistry* ContactRegistry, floa
 		int indexContactToResolve = 0;
 		int indexI = 0;
 		
-
 		if (!ContactRegistry->contacts.empty())
 		{
 			RBContact& contactToResolveInterpenatration = ContactRegistry->contacts[0];
@@ -37,113 +36,80 @@ void RBContactResolver::resolveContacts(RBContactRegistry* ContactRegistry, floa
 			ContactRegistry->contacts[indexContactToResolve].resolveInterpenetration(duration);
 			ContactRegistry->contacts[indexContactToResolve].AddImpulse(duration);
 			iterationsUsed++;
-			//ContactRegistry->contacts.erase(ContactRegistry->contacts.begin()+indexContactToResolve);
-		
 
-		//std::vector<RBContact> ContactRestants = ContactRegistry->contacts;
-		RigidBody* TempRB1 = ContactRegistry->contacts[indexContactToResolve].RigidBodies[0];
-		RigidBody* TempRB2 = ContactRegistry->contacts[indexContactToResolve].RigidBodies[1];
+			RigidBody* TempRB1 = ContactRegistry->contacts[indexContactToResolve].RigidBodies[0];
+			RigidBody* TempRB2 = ContactRegistry->contacts[indexContactToResolve].RigidBodies[1];
 
-		ContactRegistry->RemoveAllContactsFromTwoRigidBodies(TempRB1, TempRB2);
+			ContactRegistry->RemoveAllContactsFromTwoRigidBodies(TempRB1, TempRB2);
 
-				shapeRB SRB1 = TempRB1->shape; //Shape Rigidbody 1
-				shapeRB SRB2 = TempRB2->shape; //Shape Rigidbody 2
+			shapeRB SRB1 = TempRB1->shape; //Shape Rigidbody 1
+			shapeRB SRB2 = TempRB2->shape; //Shape Rigidbody 2
 
-				switch (SRB1)
-				{
+			switch (SRB1)
+			{
 				case cuboide:
 					//SRB1 CUBE
-					switch (SRB2) {
-					case cuboide:
-						RBContactGenerator::boxAndBox((PBox*)TempRB1->primitive, (PBox*)TempRB2->primitive, ContactRegistry);
-						break;
-					case sphere:
-						RBContactGenerator::boxAndSphere((PBox*)TempRB1->primitive, (PSphere*)TempRB2->primitive, ContactRegistry);
-						break;
-					case plane:
-						RBContactGenerator::boxAndPlane((PBox*)TempRB1->primitive, (PPlane*)TempRB2->primitive, ContactRegistry);
-						break;
-					default:
-						break;
+					switch (SRB2) 
+					{
+						case cuboide:
+							RBContactGenerator::boxAndBox((PBox*)TempRB1->primitive, (PBox*)TempRB2->primitive, ContactRegistry);
+							break;
+						case sphere:
+							RBContactGenerator::boxAndSphere((PBox*)TempRB1->primitive, (PSphere*)TempRB2->primitive, ContactRegistry);
+							break;
+						case plane:
+							RBContactGenerator::boxAndPlane((PBox*)TempRB1->primitive, (PPlane*)TempRB2->primitive, ContactRegistry);
+							break;
+						default:
+							break;
 					}
-					break;
+				break;
+
 				case sphere:
 					//SRB1 SPHERE
-					switch (SRB2) {
-					case cuboide:
-						RBContactGenerator::boxAndSphere((PBox*)TempRB2->primitive, (PSphere*)TempRB1->primitive, ContactRegistry);
-						break;
-					case sphere:
-						RBContactGenerator::sphereAndSphere((PSphere*)TempRB1->primitive, (PSphere*)TempRB2->primitive, ContactRegistry);
-						break;
-					case plane:
-						RBContactGenerator::sphereAndPlane((PSphere*)TempRB1->primitive, (PPlane*)TempRB2->primitive, ContactRegistry);
-						break;
-					default:
-						break;
+					switch (SRB2) 
+					{
+						case cuboide:
+							RBContactGenerator::boxAndSphere((PBox*)TempRB2->primitive, (PSphere*)TempRB1->primitive, ContactRegistry);
+							break;
+						case sphere:
+							RBContactGenerator::sphereAndSphere((PSphere*)TempRB1->primitive, (PSphere*)TempRB2->primitive, ContactRegistry);
+							break;
+						case plane:
+							RBContactGenerator::sphereAndPlane((PSphere*)TempRB1->primitive, (PPlane*)TempRB2->primitive, ContactRegistry);
+							break;
+						default:
+							break;
 					}
-					break;
+				break;
+
 				case plane:
 					//SRB1 PLANE
-					switch (SRB2) {
-					case cuboide:
-						RBContactGenerator::boxAndPlane((PBox*)TempRB2->primitive, (PPlane*)TempRB1->primitive, ContactRegistry);
-						break;
-					case sphere:
-						RBContactGenerator::sphereAndPlane((PSphere*)TempRB2->primitive, (PPlane*)TempRB1->primitive, ContactRegistry);
-						break;
-					case plane:
-						//Should exist ???
-						break;
-					default:
-						break;
+					switch (SRB2) 
+					{
+						case cuboide:
+							RBContactGenerator::boxAndPlane((PBox*)TempRB2->primitive, (PPlane*)TempRB1->primitive, ContactRegistry);
+							break;
+						case sphere:
+							RBContactGenerator::sphereAndPlane((PSphere*)TempRB2->primitive, (PPlane*)TempRB1->primitive, ContactRegistry);
+							break;
+						case plane:
+							//Should exist ???
+							break;
+						default:
+							break;
 					}
-					break;
-					break;
+				break;
+
 				default:
 					break;
-				}
+			}
 			
 		}
-		//resolveContacts(ContactRegistry, numContact, duration, potentialCollision);
-		iterationsUsed++;
+	iterationsUsed++;
 	}
-	/*
-	for (auto c : ContactRegistry->contacts)
-	{
-		Vector3D cp = Vector3D(0, 0, 0);
-		if (c.RigidBodies[0] == ContactRegistry->contacts[indexContactToResolve].RigidBodies[0]) {
 
-			cp = c.RigidBodies[0]->rotation.produitVectoriel(c.contactPoint);
-			cp += c.RigidBodies[0]->velocity; // PAs sur car pas encore faire l'impulsion
-			c.penetration -= cp.produitScalaire(c.contactNormal);
-			ContactRegistry->contacts.erase(ContactRegistry->contacts.begin() + indexContactToResolve);
-		}
-		else if (c.RigidBodies[0] == ContactRegistry->contacts[indexContactToResolve].RigidBodies[1]) {
-			cp = c.RigidBodies[1]->rotation.produitVectoriel(c.contactPoint);
-			cp += c.RigidBodies[1]->velocity; // PAs sur car pas encore faire l'impulsion
-			c.penetration -= cp.produitScalaire(c.contactNormal);
-			ContactRegistry->contacts.erase(ContactRegistry->contacts.begin() + indexContactToResolve);
-		}
-		else if(c.RigidBodies[1] == ContactRegistry->contacts[indexContactToResolve].RigidBodies[0])
-		{
-			cp = c.RigidBodies[0]->rotation.produitVectoriel(c.contactPoint);
-			cp += c.RigidBodies[0]->velocity; // PAs sur car pas encore faire l'impulsion
-			c.penetration += cp.produitScalaire(c.contactNormal);
-			ContactRegistry->contacts.erase(ContactRegistry->contacts.begin() + indexContactToResolve);
-		}
-		else if (c.RigidBodies[1] == ContactRegistry->contacts[indexContactToResolve].RigidBodies[1]) {
-			cp = c.RigidBodies[1]->rotation.produitVectoriel(c.contactPoint);
-			cp += c.RigidBodies[1]->velocity; // PAs sur car pas encore faire l'impulsion
-			c.penetration += cp.produitScalaire(c.contactNormal);
-			ContactRegistry->contacts.erase(ContactRegistry->contacts.begin() + indexContactToResolve);
-		}
-	}
-	*/
-	//ContactRegistry->RemoveContact(contactToResolveInterpenatration);
-
-
-// Updater les contacts
+	// Updater les contacts
 
 	/// Ajouter l'impulsion
 		/*
@@ -169,5 +135,4 @@ void RBContactResolver::resolveContacts(RBContactRegistry* ContactRegistry, floa
 		//ContactRegistry->RemoveContact(contactApplyImpulse);
 	}*/
 
-	
 }
